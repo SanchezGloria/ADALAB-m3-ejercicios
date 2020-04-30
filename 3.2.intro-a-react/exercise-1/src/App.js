5258cbcf442d98d513417b5909ecfa156a471dd5
SanchezGloria <gloriasancheztrillo@gmail.com>
2020-04-30 20:34:42 +0200

    add 3.2 exercise-1

---
+++ 3.2.intro-a-react/exercise-1/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import pic from './pic.jpg';
+
+import './App.scss';
+
+const userHeader = (
+  <header className="App-header">
+    <img src={pic} className="App-header-pic" alt="background" />
+    <h1 className="App-header-title">Alex Guerrero</h1>
+    <h3 className="App-header-subtitle"> Lunes 26 de junio de 2017</h3>
+  </header>
+);
+const userContent = (
+  <main className="App-main">
+    <p className="App-main-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+  </main>
+);
+const userFooter = (
+  <footer className="App-footer">
+    <p className="App-footer-readmore">Leer más...</p>
+    <span className="App-footer-likes">
+      37
+      <i className="fas fa-heart"></i>
+    </span>
+  </footer>
+);
+
+function App() {
+  const appRoot = (
+    <div className="App">
+      {userHeader},{userContent},{userFooter}
+    </div>
+  );
+  return appRoot;
+}
+
+export default App;
+
+// EJERCICIO 1
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={pic} className="App-header-pic" alt="background" />
+//     <div className="App-header-wrapper">
+//       <h1 className="App-header-title">Alex Guerrero</h1>
+//       <h3 className="App-header-subtitle">Lunes 26 de junio de 2017</h3>
+//     </div>
+//   </header>
+//   <main className="App-main">
+//     <p className="App-main-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+//   </main>
+//   <footer className="App-footer">
+//     <p className="App-footer-readmore">Leer más...</p>
+//     <span className="App-footer-likes">
+//       37
+//       <i class="fas fa-heart"></i>
+//     </span>
+//   </footer>
+// </div>
